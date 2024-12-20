4d8ff49ec4c014fc124438c3d0f9d42600e6ed40
nmc.net <kysutudongcom@gmail.com>
2024-12-20 17:35:24 +0700

    Update giao diện 50%

---
+++ Views/Converters/NumberToVisibilityConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace disaster_management.Views.Converters
+{
+    class NumberToVisibilityConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int number && parameter is string targetNumber)
+            {
+                // Kiểm tra nếu số khớp với parameter
+                if (int.TryParse(targetNumber, out int target))
+                {
+                    return number == target ? Visibility.Visible : Visibility.Collapsed;
+                }
+            }
+
+            // Mặc định là Collapsed nếu không khớp
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("ConvertBack is not implemented.");
+        }
+    }
+}
---
+++ Views/Usercontrols/DiseaseManagement.xaml.cs
@@ -0,0 +1,41 @@
+﻿using disaster_management.Views.SubWindows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace disaster_management.Views.Usercontrols
+{
+    /// <summary>
+    /// Interaction logic for DiseaseManagement.xaml
+    /// </summary>
+    public partial class DiseaseManagement : UserControl
+    {
+        public DiseaseManagement()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_ChonLoaiDichBenh(object sender, RoutedEventArgs e)
+        {
+           SelectDisease selectDisease = new SelectDisease();
+            selectDisease.ShowDialog();
+        }
+
+        private void Button_ChonODichBenh(object sender, RoutedEventArgs e)
+        {
+            SelectOutbreak selectOutbreak = new SelectOutbreak();
+            selectOutbreak.ShowDialog();
+        }
+    }
+}
