339f1ce5f7463f02837192f566a0e678cc162929
kzxl <65649029+kzxl@users.noreply.github.com>
2024-12-24 10:11:42 +0700

    cập nhật UI hệ thống

--- Views/Usercontrols/SystemManagement.xaml.cs
+++ Views/Usercontrols/SystemManagement.xaml.cs
@@ -24,5 +24,30 @@ namespace disaster_management.Views.Usercontrols
         {
             InitializeComponent();
         }
+        private void ComboBoxAdminstrativeSection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboboxAdministrative.SelectedItem is ComboBoxItem selectedItem)
+            {
+                string level = selectedItem.Content.ToString();
+                lbAdminstartiveName.Text = $"Tên {level}:";
+                lbAdminstartiveName_Edit.Text = $"Tên {level}:";
+                // Nếu chọn "Tỉnh", ẩn ComboBoxParent
+                if (level == "Tỉnh")
+                {
+                    ParentSelectionPanel.Visibility = Visibility.Collapsed;
+                    ParentSelectionPanel_Edit.Visibility = Visibility.Collapsed;
+                }
+                else
+                {
+                    ParentSelectionPanel.Visibility = Visibility.Visible;
+                    ParentSelectionPanel_Edit.Visibility=Visibility.Visible;
+                    // Hiển thị danh sách dựa trên cấp độ
+                   //load dữ liệu
+                }
+            }
+        }
+        // Hàm lấy danh sách cấp trên
+       
     }
+}
 }
